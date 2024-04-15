6a33bbc2437d2cdae4d022c253ea2f7f75f97fd9
NotoriousSledge <n.jakob.bergstrom@outlook.com>
2024-04-15 20:36:12 +0200

    thread sync

--- packages/core/src/thread_sync/thread_sync.ts
+++ packages/core/src/thread_sync/thread_sync.ts
@@ -1,4 +1,44 @@
-import type {PartitionMap} from '@/lib';
+import type {PartionedMap} from '@/lib';
+
 export class ThreadSync {
-  #state: PartitionMap<Promise<any>>;
+  #state: PartionedMap<Promise<any>> = new Map();
+
+  readonly run = <T>(
+    generator: () => Promise<T>,
+    partition_key: string,
+    sort_keys: Array<string>,
+    ttl?: number,
+  ): Promise<T> => {
+    const sort_key = this.#format_sort_key(sort_keys);
+    const cached = this.#state.get(partition_key)?.get(sort_key);
+    if (cached) {
+      return cached as Promise<T>;
+    }
+
+    const promise = generator();
+    if (this.#state.has(partition_key)) {
+      this.#state.get(partition_key)!.set(sort_key, promise);
+    } else {
+      this.#state.set(partition_key, new Map([[sort_key, promise]]));
+    }
+
+    if (typeof ttl === 'number') {
+      setTimeout(() => this.#state.get(partition_key)!.delete(sort_key), ttl);
+    }
+
+    return promise;
+  };
+
+  readonly has = (partition_key: string, sort_keys: Array<string>): boolean => {
+    return Boolean(
+      this.#state.get(partition_key)?.has(this.#format_sort_key(sort_keys)),
+    );
+  };
+
+  readonly reset = (partition_key: string, sort_keys: Array<string>): void => {
+    this.#state.get(partition_key)?.delete(this.#format_sort_key(sort_keys));
+  };
+
+  readonly #format_sort_key = (sort_keys: Array<string>) =>
+    sort_keys.sort().join('#');
 }
